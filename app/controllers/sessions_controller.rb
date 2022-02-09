7ec1fc8c7c990bf9689b8ebce615effdeec88965
Matthew Puku <matthew.puku@gmail.com>
2022-02-09 14:34:28 +1000

    tighten up the graphics

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,7 @@ class SessionsController < ApplicationController
   def create
     jws = params.require(:assertion).to_s
     secret = RapidConnection.find(params.require(:rapid_connection_id)).secret
-    jwt = JSON::JWT.decode(jws, secret)
-
-    render json: jwt
+    @jwt = JSON::JWT.decode(jws, secret)
   rescue StandardError => e
     render json: "#{e.class}\n\n#{e.message}"
   end
